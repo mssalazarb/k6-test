1b7fbdfaa31eb928c81597a67e7f337bf74b2e00
Michael Salazar <michael.salazar@rappi.com>
2022-08-30 10:20:55 -0500

    Dinamic test with k6 and backend api

--- k6/test.js
+++ k6/test.js
@@ -10,65 +10,37 @@ export let options = {
       exec: "normal",
       gracefulStop: '0s',
     },
-    notFound: {
-      executor: 'constant-vus',
-      vus: 200,
-      duration: '10s',
-      exec: "notFound",
-      gracefulStop: '0s',
-      startTime: '10s'
-    },
-    error: {
-      executor: 'constant-vus',
-      vus: 200,
-      duration: '10s',
-      exec: "error",
-      gracefulStop: '0s',
-      startTime: '20s'
-    },
-    slow: {
-      executor: 'constant-vus',
-      vus: 200,
-      duration: '10s',
-      exec: "slow",
-      gracefulStop: '10s',
-      startTime: '30s'
-    },
-    verySlow: {
-      executor: 'constant-vus',
-      vus: 200,
-      duration: '10s',
-      exec: "verySlow",
-      gracefulStop: '30s',
-      startTime: '50s'
-    }
   }
 };
 
 const host = "http://127.0.0.1:3000";
 
 export function normal() {
-  http.get(host + "/users/4/balance");
-  sleep(0.5);
-}
+  http.get(host + "/users/4/balance", {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQiLCJyb2xlIjoidXNlciIsImlhdCI6MTY2MDA2OTQ5NCwiZXhwIjoxNjYwNjc0Mjk0fQ.Q8DrVV8j7aY2_TWZHtVUarO8m5MFqIIpQZkzwE3wwOI'
+    },
+  });
 
-export function slow() {
   http.post(host + "/users/4/transactions/deposit", JSON.stringify({
     "cardId": 23,
     "amount": 100
-  }));
-  sleep(0.5);
-}
+  }), {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQiLCJyb2xlIjoidXNlciIsImlhdCI6MTY2MDA2OTQ5NCwiZXhwIjoxNjYwNjc0Mjk0fQ.Q8DrVV8j7aY2_TWZHtVUarO8m5MFqIIpQZkzwE3wwOI'
+    },
+  });
 
-export function verySlow() {
-  http.get(host + "/users/4/transactions/transfer", JSON.stringify({
+  http.post(host + "/users/4/transactions/transfer", JSON.stringify({
     "creditUserId": 12,
     "amount": 100
-  }));
-  sleep(0.5);
-}
-
-export function notFound() {
-  http.get(host + "/users/4/transactions?order=ASC&page=1&take=10");
+  }), {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQiLCJyb2xlIjoidXNlciIsImlhdCI6MTY2MDA2OTQ5NCwiZXhwIjoxNjYwNjc0Mjk0fQ.Q8DrVV8j7aY2_TWZHtVUarO8m5MFqIIpQZkzwE3wwOI'
+    },
+  });
   sleep(0.5);
 }
